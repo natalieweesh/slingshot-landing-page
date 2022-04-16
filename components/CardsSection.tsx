fc902dd7619ff813056df4314e3c57ef3ca3c27d
Natalie Wee <natalie@codecademy.com>
2022-04-16 08:47:50 -0400

    tabs => spaces

--- components/CardsSection.tsx
+++ components/CardsSection.tsx
@@ -61,22 +61,22 @@ const fakeCardData = [
 ];
 
 export const CardsSection = () => {
-	return (
-		<div className={styles.cardsSection}>
-			<div className={styles.cardsSectionText}>
-				<header className={cn(styles.header2, styles.smallCenter)}>Explore and trade thousands of cryptocurrencies</header>
-				<p className={cn(styles.headerText, styles.smallCenter)}>From Ethereum to Apecoin, there are 7,500+ tokens available to trade at the best prices and lowest fees.</p>
-			</div>
-			<div className={styles.cards}>
-				{fakeCardData.map((card) => {
-					return <div className={styles.card}>
-						<Image className={styles.coinImage} src={card.img} alt={card.name} width={60} height={60} />
-						<span className={styles.coinTitle}><strong>{card.name}</strong></span>
-						<span className={styles.coinPrice}>${card.price}</span>
-						<p><div className={cn({[styles.arrowContainer]: true, [styles.upsideDown]: card.change < 0})}><Image className={styles.arrow} src="/images/arrow.svg" alt='arrow' width='100%' height='100%' layout='responsive' /></div>{' '}{card.change}%</p>
-					</div>
-				})}
-			</div>
-		</div>
-	)
+  return (
+    <div className={styles.cardsSection}>
+      <div className={styles.cardsSectionText}>
+        <header className={cn(styles.header2, styles.smallCenter)}>Explore and trade thousands of cryptocurrencies</header>
+        <p className={cn(styles.headerText, styles.smallCenter)}>From Ethereum to Apecoin, there are 7,500+ tokens available to trade at the best prices and lowest fees.</p>
+      </div>
+      <div className={styles.cards}>
+        {fakeCardData.map((card) => {
+          return <div className={styles.card}>
+            <Image className={styles.coinImage} src={card.img} alt={card.name} width={60} height={60} />
+            <span className={styles.coinTitle}><strong>{card.name}</strong></span>
+            <span className={styles.coinPrice}>${card.price}</span>
+            <p><div className={cn({[styles.arrowContainer]: true, [styles.upsideDown]: card.change < 0})}><Image className={styles.arrow} src="/images/arrow.svg" alt='arrow' width='100%' height='100%' layout='responsive' /></div>{' '}{card.change}%</p>
+          </div>
+        })}
+      </div>
+    </div>
+  )
 }
--- components/TwitterSection.tsx
+++ components/TwitterSection.tsx
@@ -48,23 +48,23 @@ const fakeTwitterData: TwitterCardContent[] = [
 ]
 
 export const TwitterSection = () => {
-	return (
-		<>
-			<div className={styles.twitterSectionBigger}>
-			{fakeTwitterData.map((card) => {
-				return <TwitterCard card={card} />
-			})}
-			</div>
-			<div className={styles.twitterSectionTop}>
-				{fakeTwitterData.slice(0, fakeTwitterData.length/2).map((card) => {
-					return <TwitterCard card={card} />
-				})}
-			</div>
-			<div className={styles.twitterSectionBottom}>
-				{fakeTwitterData.slice(fakeTwitterData.length/2).map((card) => {
-					return <TwitterCard card={card} />
-				})}
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className={styles.twitterSectionBigger}>
+      {fakeTwitterData.map((card) => {
+        return <TwitterCard card={card} />
+      })}
+      </div>
+      <div className={styles.twitterSectionTop}>
+        {fakeTwitterData.slice(0, fakeTwitterData.length/2).map((card) => {
+          return <TwitterCard card={card} />
+        })}
+      </div>
+      <div className={styles.twitterSectionBottom}>
+        {fakeTwitterData.slice(fakeTwitterData.length/2).map((card) => {
+          return <TwitterCard card={card} />
+        })}
+      </div>
+    </>
+  )
 }
