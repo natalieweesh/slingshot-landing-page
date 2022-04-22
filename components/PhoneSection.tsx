a1587d2c4c5687b873165b2e835192051e84764e
Natalie Wee <natalie@codecademy.com>
2022-04-21 22:58:48 -0400

    change back to non sticky phone parallax

--- components/PhoneSection.tsx
+++ components/PhoneSection.tsx
@@ -9,7 +9,7 @@ export const PhoneSection = () => {
     <ParallaxProvider>
       <div className={cn(styles.innerContainer, styles.noPaddingBottom)}>
         <div className={styles.phoneSectionBigger}>
-          <div className={styles.phoneWrapper}><Image className={styles.phone} src="/images/phone.svg" width='100%' height='100%' layout='responsive' /></div>
+          <div className={styles.phoneWrapper}><Image className={styles.phone} src="/images/phone.svg" width={360} height={730} /></div>
           <Parallax className={cn(styles.parallaxSquare, styles.square1)} speed={20}><div className={cn(styles.square, styles.largeSquare)}><Image src="/images/square1.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
           <Parallax className={cn(styles.parallaxSquare, styles.square2)} speed={15}><div className={cn(styles.square, styles.mediumSquare)}><Image src="/images/square2.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
           <Parallax className={cn(styles.parallaxSquare, styles.square3)} speed={8}><div className={styles.square}><Image src="/images/square3.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
@@ -17,23 +17,9 @@ export const PhoneSection = () => {
           <Parallax className={cn(styles.parallaxSquare, styles.square5)} speed={15}><div className={cn(styles.square, styles.largeSquare)}><Image src="/images/square5.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
           <Parallax className={cn(styles.parallaxSquare, styles.square6)} speed={15}><div className={cn(styles.square, styles.mediumSquare)}><Image src="/images/square6.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
           <Parallax className={cn(styles.parallaxSquare, styles.square7)} speed={8}><div className={styles.square}><Image src="/images/square7.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square8)} speed={8}><div className={cn(styles.square, styles.mediumSquare)}><Image src="/images/square8.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
+          <Parallax className={cn(styles.parallaxSquare, styles.square8)} speed={15}><div className={cn(styles.square, styles.mediumSquare)}><Image src="/images/square8.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
           <Parallax className={cn(styles.parallaxSquare, styles.square9)} speed={8}><div className={styles.square}><Image src="/images/square9.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
         </div>
-      </div>
-      <div id="phone-section" className={styles.phoneSection}>
-        <div className={cn(styles.sticky)}>
-          <div className={styles.phone}><Image src="/images/phone.svg" width={360} height={730} /></div>
-        </div>
-          <Parallax className={cn(styles.parallaxSquare, styles.square1)} speed={20}><div className={cn(styles.square, styles.largeSquare)}><Image src="/images/square1.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square2)} speed={15}><div className={cn(styles.square, styles.mediumSquare)}><Image src="/images/square2.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square3)} speed={8}><div className={styles.square}><Image src="/images/square3.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square4)} speed={8}><div className={styles.square}><Image src="/images/square4.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square5)} speed={15}><div className={cn(styles.square, styles.largeSquare)}><Image src="/images/square5.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square6)} speed={15}><div className={cn(styles.square, styles.mediumSquare)}><Image src="/images/square6.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square7)} speed={8}><div className={styles.square}><Image src="/images/square7.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square8)} speed={8}><div className={cn(styles.square, styles.mediumSquare)}><Image src="/images/square8.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
-          <Parallax className={cn(styles.parallaxSquare, styles.square9)} speed={8}><div className={styles.square}><Image src="/images/square9.svg" layout="responsive" width="100%" height="100%" /></div></Parallax>
       </div>
     </ParallaxProvider>
   )
